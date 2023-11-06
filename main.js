07c06008ac4f76b26b4f5f84740c0ecadae75d5a
Julian <127077495+Scythe101@users.noreply.github.com>
2023-11-06 21:06:40 +0000

    changed bg

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+var isOpen=false;
+var isOver=false;
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  isOpen=true;
+}
   
-  function closeNav() {
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  isOpen=false;
+}
+
+function teaseNav(){
+  document.getElementById("mySidenav").style.width = "7px";
+}
+
+function closeTeasedNav(){
+  if (isOpen==false){
     document.getElementById("mySidenav").style.width = "0";
   }
+}
